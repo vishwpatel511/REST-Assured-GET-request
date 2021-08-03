6b883baeaec70190e88c4fe22270c0fa3a7fab12
vishwpatel511 <vishwpatel511@gmail.com>
2021-08-03 17:28:58 -0400

    one liner way to get the response and display it to console

--- src/test/java/com/qa/APITestFunction/getCallTest.java
+++ src/test/java/com/qa/APITestFunction/getCallTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers.*;
+import org.json.simple.JSONObject;
 
 
 public class getCallTest {
@@ -12,20 +14,46 @@ public class getCallTest {
   @Test
   public void f() {
 	  
-	 Response res = RestAssured.get("https://reqres.in/api/users?page=2");
-	 System.out.println(res.getStatusCode());
-	 System.out.println(res.getStatusLine());
-	 System.out.println(res.getCookies());
-	 Assert.assertEquals(200, res.getStatusCode());
-  }
+//	 Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+	 
+	 RestAssured.given().get("https://reqres.in/api/users?page=2").then().log().all();
+		/*
+		 * System.out.println(res.asPrettyString()); System.out.println(res.headers());
+		 * System.out.println(res.getStatusCode());
+		 * System.out.println(res.getStatusLine());
+		 * System.out.println(res.getCookies()); Assert.assertEquals(200,
+		 * res.getStatusCode());
+		 */  }
   
   @Test
   public void test002() {
 	  
-	  Response res = RestAssured.get("https://reqres.in/api/users?page=2");
-		 System.out.println(res.getStatusCode());
-		 System.out.println(res.getStatusLine());
-		 System.out.println(res.getCookies());
-		 Assert.assertEquals(7, res.jsonPath().get("data.id[0]"));
+//	  Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+	 
+	  
+	  
+	  
+	
+	  
+		/*
+		 * System.out.println(res.getStatusCode());
+		 * 
+		 * JSONObject object = new JSONObject(); object.put("name", "Vishw");
+		 * object.put("job", "Pilot");
+		 * 
+		 * System.out.println(object);
+		 * 
+		 * RestAssured.given().body(object.toJSONString()).then().statusCode(404);
+		 * 
+		 * System.out.println(res.getStatusLine());
+		 * System.out.println(res.getCookies());
+		 * System.out.println(res.jsonPath().get("data.last_name[5]"));
+		 * System.out.println(res.getHeaders());
+		 * System.out.println(res.getBody().asPrettyString());
+		 * Assert.assertEquals("Howell", res.jsonPath().get("data.last_name[5]")); //
+		 * System.out.println(res.body().asPrettyString()..contains("Micheal"));
+		 */	
+		 
+		 
   }
 }
