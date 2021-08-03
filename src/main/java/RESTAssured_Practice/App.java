0169a58e4f981a376927841024d0c2b5c4a24ed5
vishwpatel511 <vishwpatel511@gmail.com>
2021-08-03 15:27:06 -0400

    initial commit

---
+++ src/main/java/RESTAssured_Practice/App.java
@@ -0,0 +1,13 @@
+package RESTAssured_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
